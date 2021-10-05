ab37426fa3e929916f2ed876871483669cbfe9b5
Christian Colon <christian.colon27@gmail.com>
2021-10-05 14:05:35 -0400

    added post route

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,18 @@ router.get("/notes", (req, res) => {
     res.json(notesArray);
 });
 
-
+router.post("/notes", (req, res) => {
+    const notes = fs.readFileSync('db/db.json', 'utf8');
+    const notesArray = JSON.parse(notes);
+    res.json(notesArray);
+    const notesList = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4()
+    }
+    const newNote = notesArray.concat(notesList);
+    const newList = fs.writeFileSync('db/db.json', JSON.stringify(newNote));
+    res.json(newList);
+})
 
 module.exports = router;
